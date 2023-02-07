6f35adf65dcf6075498698ec9c03603c8cb3f11b
handryos <74721057+handryos@users.noreply.github.com>
2023-02-07 17:37:29 -0300

    Update Dados.test.ts

--- test/Dados.test.ts
+++ test/Dados.test.ts
@@ -1,11 +1,11 @@
 import Dados from "../src/Dados"
 
-// test("Deve ler os dados em uma lista e retorna-los multiplica-los", function(){
-//     const dados =  new Dados(10, 20, 30, 40, 50);
-//     const teste = dados.multiplicar();
-//     const resultado = dados.lista;
-//     expect(teste).toEqual([100, 200, 300, 400, 500]);
-// })
+test("Deve ler os dados em uma lista e retorna-los multiplica-los", function(){
+    const dados =  new Dados(10, 20, 30, 40, 50);
+    const teste = dados.multiplicar();
+    const resultado = dados.lista;
+    expect(teste).toEqual([100, 200, 300, 400, 500]);
+})
 
 test("Deve ler os dados em uma lista e validar caso o tamanho seja autentico", function(){
     const dados = new Dados(10, 20, 30,40,50);
