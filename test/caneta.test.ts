3b4662f1a4dd0904dd7d88ff8d9319afcab1f5b9
handryos <74721057+handryos@users.noreply.github.com>
2023-02-07 17:36:43 -0300

    Update caneta.test.ts

--- test/caneta.test.ts
+++ test/caneta.test.ts
@@ -1,14 +1,14 @@
-// import Caneta from "../src/Caneta";
+import Caneta from "../src/Caneta";
 
-// test("Deve escrever algo", function () {
-//     const caneta = new Caneta();
-//     const text = caneta.escrever('Olá mundo!');
-//     expect(text).toBe("Olá mundo!");
-// });
+test("Deve escrever algo", function () {
+    const caneta = new Caneta();
+    const text = caneta.escrever('Olá mundo!');
+    expect(text).toBe("Olá mundo!");
+});
 
-// test("Caneta deve perder tina quando escrever", function () {
-//     const caneta = new Caneta();
-//     expect(caneta.tinta).toBe(20);
-//     caneta.escrever('Olá mundo!');
-//     expect(caneta.tinta).toBe(10);
-// });
+test("Caneta deve perder tina quando escrever", function () {
+    const caneta = new Caneta();
+    expect(caneta.tinta).toBe(20);
+    caneta.escrever('Olá mundo!');
+    expect(caneta.tinta).toBe(10);
+});
